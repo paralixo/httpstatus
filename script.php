b15f4c76eef9d64c6b02002cd0a842e6b0e4dca6
paralixo <florian.lafuente@ynov.com>
2019-02-21 17:33:24 +0100

    correction script

--- script.php
+++ script.php
@@ -1,29 +1,35 @@
 <?php
 
-//while (true){
 $database_host = 'localhost';
 $database_port = '8080';
 $database_dbname = 'lafuente_mehaye';
 $database_user = 'root';
 $database_password = 'bernardbernard';
 $database_charset = 'UTF8';
+$database_options = [
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+];
 
 $pdo = new PDO(
-	'mysql:host=' . $database_host .
-	';port=' . $database_port .
-	';dbnale=' . $database_dbname .
-	';charset=' . $database_charset,
-	$database_user,
-	$database_password
+    'mysql:host=' . $database_host . 
+    ';port=' . $database_port . 
+    ';dbname=' . $database_dbname . 
+    ';charset=' . $database_charset, 
+    $database_user,
+    $database_password,
+    $database_options
 );
 
 $query = $pdo->prepare('SELECT * FROM websites');
 $query->execute();
 $url = $query->fetchAll();
-var_dump($pdo);
+
 foreach ($url as $website)
 {
 	echo 'Url is : ' .$website['url'] . "\n";
+	$output = shell_exec("curl -sS --head " . $website['url']);
+echo $output;
 }
 //}
 
