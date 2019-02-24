e3654c82e21873f7c592af620d4d9afa2a24b2c2
paralixo <florian.lafuente@ynov.com>
2019-02-24 18:50:40 +0100

    script fini

--- script.php
+++ script.php
@@ -21,16 +21,44 @@ $pdo = new PDO(
     $database_options
 );
 
+while (true){
 $query = $pdo->prepare('SELECT * FROM websites');
 $query->execute();
 $url = $query->fetchAll();
 
 foreach ($url as $website)
 {
-	echo 'Url is : ' .$website['url'] . "\n";
-	$output = shell_exec("curl -sS --head " . $website['url']);
-echo $output;
+	$output = shell_exec("curl -s -o/dev/null " . $website['url'] . " -w '%{http_code}\n'");
+
+
+	$query = $pdo->prepare('INSERT INTO history (id_website,status,update_time)values('.$website['id'].','.$output.',now())');
+	$query->execute();
+	
 }
-//}
+foreach ($url as $website)
+{
+	$id = $website['id'];
+	$query = $pdo->prepare('SELECT status from history where id_website = '.$id.' order by update_time desc limit 3 ');
+	$query->execute();
+	$laststatus = $query->fetchAll();
+
+	if (count($laststatus) === 3 ){
+		$websitedown = true ;
+		foreach ($laststatus as $status) {
+			// on prends 400 à la place de 300 parce qu'on ne sait pas comment tu vas ecrire tes noms de domaines.
+			if ($status['status'] >= 200 && $status['status'] < 400 ){ 
+				$websitedown =false;
+			}
+		}
+		if($websitedown == true ){
+			mail('deschaussettes@yopmail.com','Erreur site','Probleme sur le site '. $website['url'] .'');
+		}
+	}
+
+}
+	
+sleep (120);
+
+}
 
 ?>
